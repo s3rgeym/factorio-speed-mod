d884df3ef65e043ce07d321b2925ae9fa40de2fa
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2019-11-18 11:59:48 +0300

    Update gamespeed.lua

--- gamespeed.lua
+++ gamespeed.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 M.MODIFIERS = { 0.25, 0.5, 0.75, 1, 1.5, 2, 4 }
-M.DEFAULT = M.MODIFIERS[3] 
+M.DEFAULT = M.MODIFIERS[4] 
 
 function M.next(cur)
-  for i = 1, #M.MODIFIERS do
-    if M.MODIFIERS[i] > cur then
-      return M.MODIFIERS[i]
+  for _, v in ipairs(M.MODIFIERS) do
+    if v > cur then
+      return v
     end
   end
   return cur
